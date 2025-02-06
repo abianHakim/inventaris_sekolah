7b7aa98ab91a1a850caf91a33c58fcf024ab1553
abianHakim <abianhakim09@gmail.com>
2025-02-06 21:08:50 +0700

    mengubah halaman login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -11,8 +10,6 @@
 
     <title>Inventaris Sekolah - Login</title>
     <link rel="icon" type="image/jpeg" href="{{ url('assets/img/favicon.jpg') }}">
-
-
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('assets') }}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -23,70 +20,88 @@
     <!-- Custom styles for this template-->
     <link href="{{ asset('assets') }}/css/sb-admin-2.min.css" rel="stylesheet">
 
+    <style>
+        body {
+            background: linear-gradient(to right, #3b565c, #214460);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        .card {
+            border-radius: 12px;
+            box-shadow: 0px 10px 30px rgba(0, 0, 0, 0.1);
+            border: none;
+        }
+
+        .btn-primary {
+            background-color: #4a69bd;
+            border-color: #4a69bd;
+            transition: all 0.3s ease;
+        }
+
+        .btn-primary:hover {
+            background-color: #3b5998;
+            border-color: #3b5998;
+        }
+
+        .form-control {
+            border-radius: 8px;
+            padding: 14px;
+            font-size: 16px;
+        }
+
+        .login-logo {
+            width: 60%;
+            max-width: 200px;
+        }
+    </style>
 </head>
 
-<body class="bg-gradient-primary">
+<body>
 
     <div class="container">
 
-        <!-- Outer Row -->
-        <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="d-flex justify-content-center align-items-center">
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-lg-6 col-md-8 col-sm-10">
+                <div class="card o-hidden border-0 shadow-lg">
+                    <div class="card-body p-5">
+                        <div class="text-center">
+                            <img src="{{ asset('assets/img/logo.login.svg') }}" alt="Logo" class="login-logo mb-3">
+                            <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                        </div>
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-6 d-flex justify-content-center align-items-center bg-login-image "
-                                style="background-position: center; background-size: contain; background-repeat: no-repeat;">
-                                <img src="{{ asset('assets/img/logo.login.svg') }}" alt="" class="img-fluid"
-                                    style="max-width: 70%">
+                        <form class="user" action="{{ route('login') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user"
+                                    placeholder="Enter Username" name="user_name" required>
                             </div>
-
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                                    </div>
-                                    <form class="user" action="{{ route('login') }}" method="POST">
-                                        @csrf
-                                        <div class="form-group">
-                                            <input type="text" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter User Name" name="user_name">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password" name="user_pass">
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
-                                            </div>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </button>
-
-                                    </form>
-                                    <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot
-                                            Password?</a>
-                                    </div>
-                                    {{-- <div class="text-center">
-                                        <a class="small" href="{{ route('register') }}">Create an
-                                            Account!</a>
-                                    </div> --}}
+                            <div class="form-group">
+                                <input type="password" class="form-control form-control-user" placeholder="Password"
+                                    name="user_pass" required>
+                            </div>
+                            <div class="form-group">
+                                <div class="custom-control custom-checkbox small">
+                                    <input type="checkbox" class="custom-control-input" id="customCheck">
+                                    <label class="custom-control-label" for="customCheck">Remember Me</label>
                                 </div>
                             </div>
+                            <button type="submit" class="btn btn-primary btn-user btn-block">
+                                Login
+                            </button>
+                        </form>
+
+                        <hr>
+
+                        <div class="text-center">
+                            <a class="small" href="forgot-password.html">Forgot Password?</a>
                         </div>
+
                     </div>
                 </div>
-
             </div>
 
         </div>
